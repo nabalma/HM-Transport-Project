97d4e8f5d5d190143c3edb59654ed8c2c64bd480
nabalma <nabalma_hussein@yahoo.fr>
2022-11-17 18:21:06 -0500

    Depot de chargement  Table and Class Creation

---
+++ administrateur/depotsChargements/creationTableDepotsChargements.php
@@ -0,0 +1,19 @@
+<?php
+
+require "depotChargement.php";
+$depotChargementObj=new DepotChargement(); 
+
+$Req= "CREATE TABLE IF NOT EXISTS depotsChargements (
+Ref_Depot_Chargement INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+Designation_Depot_Chargement VARCHAR(20) NOT NULL ,
+Duree_Trajet_allee_Chargement_Target DECIMAL NOT NULL ,
+Duree_Avant_Chargement_Target DECIMAL NOT NULL ,
+Duree_Chargement_Target DECIMAL NOT NULL ,
+Duree_Formalite_Apres_Chargement_Target DECIMAL NOT NULL ,
+Duree_Trajet_Retour_DepotFrontiere_Target DECIMAL NOT NULL ,
+Duree_Formalite_Transit_Frontière_Target DECIMAL NOT NULL ,
+Duree_Trajet_Retour_FrontiereArrivee_Target DECIMAL NOT NULL)";
+
+$depotChargementObj->con->query($Req);
+
+?>
---
+++ administrateur/depotsChargements/depotChargement.php
@@ -0,0 +1,55 @@
+<?php
+
+class DepotChargement{
+
+    // Cobfigurations DB
+    private $servername = '127.0.0.1';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'db_transport';
+    public $con;
+    // Database Connection
+    public function __construct(){
+        $this->con = new mysqli($this->servername, $this->username,
+         $this->password, $this->database);
+         if(mysqli_connect_error()){
+            trigger_error("Failed to connect MYSQL: " .mysqli_connect_error());
+         }
+         else {
+           return $this->con;            
+         }
+    }
+    // Insert data
+    public function insertData($post){
+
+        $var_Designation_Depot_Chargement_target= $this->con->real_escape_string($_POST['designation']);
+        $var_Duree_Trajet_allee_Chargement_Target= $this->con->real_escape_string($_POST['dureeTrajetAlleeChargementTarget']);
+        $var_Duree_Avant_Chargement_Target=$this->con->real_escape_string($_POST['dureeAvantChargementTarget']);
+        $var_Duree_Chargement_Target= $this->con->real_escape_string($_POST['dureeChargementTarget']);
+        $var_Duree_Formalite_Apres_Chargement_Target= $this->con->real_escape_string($_POST['dureeFormaliteApresChargementTarget']);
+        $var_Duree_Trajet_Retour_DepotFrontiere_Target= $this->con->real_escape_string($_POST['dureeTrajetRetourVersFrontiereTarget']);
+        $var_Duree_Formalite_Transit_Frontière_Target= $this->con->real_escape_string($_POST['dureeFormaliteTransitFrontiereTarget']);
+        $var_Duree_Trajet_Retour_FrontiereArrivee_Target=$this->con->real_escape_string($_POST['dureeTrajetRetourFrontiereArriveeTarget']);
+
+   
+        $new_query = "INSERT INTO depotsChargements  (Designation_Depot_Chargement, Duree_Trajet_allee_Chargement_Target, Duree_Avant_Chargement_Target, Duree_Chargement_Target, Duree_Formalite_Apres_Chargement_Target, Duree_Trajet_Retour_DepotFrontiere_Target, Duree_Formalite_Transit_Frontière_Target, Duree_Trajet_Retour_FrontiereArrivee_Target)
+        VALUES('$var_Designation_Depot_Chargement_target',$var_Duree_Trajet_allee_Chargement_Target,$var_Duree_Avant_Chargement_Target,$var_Duree_Chargement_Target, $var_Duree_Formalite_Apres_Chargement_Target,$var_Duree_Trajet_Retour_DepotFrontiere_Target,$var_Duree_Formalite_Transit_Frontière_Target, $var_Duree_Trajet_Retour_FrontiereArrivee_Target)";
+
+
+        $result = $this->con->query($new_query);
+        if($result){
+            echo 'Object inserted successfully! <br/>';
+        }else{
+            echo 'Failed to register, try again!';
+        }
+    }
+
+
+
+    
+
+
+
+}
+
+?>
